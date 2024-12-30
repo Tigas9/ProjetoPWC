512e30f3ae4739ce1686c72074b1b27e348d3afc
Isabel Costa <146230706+isabel-costa@users.noreply.github.com>
2024-12-30 18:03:02 +0000

    Função checkFavorito com local storage

--- assets/js/paises.js
+++ assets/js/paises.js
@@ -91,4 +91,9 @@ function displayCountries(arrayCountries){
         var favoritos = localStorage.getItem("favoritos");
         return favoritos ? JSON.parse(favoritos) : [];
     }
+
+        function checkFavorito(countryCode) {
+        var favoritos = getFavoritos();
+        return favoritos.includes(countryCode);
+    }
 }
